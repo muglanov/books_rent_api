3e9dcc85072cdb19509567bf544632d7fc79f848
muglanov <muglanov_artem@mail.ru>
2017-12-20 00:08:08 +0300

    query for books table in test

--- test/fill_test_data.py
+++ test/fill_test_data.py
@@ -14,6 +14,13 @@ def insert_user_query(username, password, money):
     ({}, {}, {}, {})
     '''.format(username, salted_pwd, salt, money)
 
+def insert_book_query(name, author, rent_price):
+    return '''INSERT INTO book 
+        (author, name, rent_price) VALUES 
+        ({}, {}, {}, {})
+        '''.format(author, name, rent_price)
+
 with psycopg2.connect('dbname="books_rent" user="postgres" host="localhost" password="12345678"') as conn:
     cur = conn.cursor()
     cur.execute(insert_user_query('Василий', '123456', 7500))
+    cur.execute(insert_book_query('Гари Поттер и философский камень', 'Джоан Роулинг', 150))
