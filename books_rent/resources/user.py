0565e12360062cbcad638ffce10e759eafb8fdf0
muglanov <muglanov_artem@mail.ru>
2017-12-19 23:35:47 +0300

    add login and logout user api

--- books_rent/resources/user.py
+++ books_rent/resources/user.py
@@ -4,26 +4,32 @@ from flask_restful import Resource, request
 
 from books_rent import authorized_users
 from books_rent.models import User as UserModel
+from books_rent.utils import unique_token
 
 
 class UserSignIn(Resource):
 
-    def put(self):
-        username = request.form['username']
-        password = request.form['password']
-        user = UserModel.query.filter_by(username=username, password=password).first()
-        if user:
-            token = 'token'
-            authorized_users[user] = token
-            return {'token': token}
-        return {'token': None}, 401
+    def get(self):
+        username = request.args.get('username')
+        password = request.args.get('password')
+        if username is None or password is None:
+            return {'status': False, 'error': 'Username or password is none'}, 401
+        user = UserModel.query.filter_by(username=username).first()
+        if not user:
+            return {'status': False, 'error': 'User with this username'}, 401
+        salt = user.salt
+        salted_pwd = '{}{}'.format(salt, password)
+        if salted_pwd != user.password:
+            return {'status': False, 'error': 'User with this username and password not exist'}, 401
+        token = unique_token()
+        authorized_users[token] = user
+        return {'status': True, 'token': token, 'error': None}
 
 
 class UserSignOut(Resource):
 
-    def put(self):
-        username = request.form['username']
-        token = request.form['token']
-        if authorized_users.get(username) and authorized_users[username] == token:
-            del authorized_users[username]
-        return {'result': 'ok'}
+    def get(self):
+        token = request.args.get('token')
+        if token and authorized_users.get(token):
+            del authorized_users[token]
+        return {'status': True, 'error': None}
--- books_rent/utils.py
+++ books_rent/utils.py
@@ -1,7 +1,14 @@
 # coding: utf-8
 
+from hashlib import sha1
+from random import random
+
 POSTGRES = 'postgresql'
 
 
 def get_postgresql_db_uri(user, password, db_name, host='localhost'):
     return '{}://{}:{}@{}/{}'.format(POSTGRES, user, password, host, db_name)
+
+
+def unique_token():
+    return sha1(str(random()).encode()).hexdigest()
