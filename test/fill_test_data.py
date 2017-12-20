c768c7c7ae5c3fa8f651edfc481d459690a5ac5f
muglanov <muglanov_artem@mail.ru>
2017-12-20 10:35:18 +0300

    fix postgres query

--- test/fill_test_data.py
+++ test/fill_test_data.py
@@ -9,18 +9,19 @@ def insert_user_query(username, password, money):
     salt = sha1(str(random()).encode()).hexdigest()
     prepeared_pwd = '{}{}'.format(salt, password)
     salted_pwd = sha1(prepeared_pwd.encode()).hexdigest()
-    return '''INSERT INTO user 
+    return '''INSERT INTO public.user 
     (username, password, salt, money) VALUES 
-    ({}, {}, {}, {})
+    ('{}', '{}', '{}', {})
     '''.format(username, salted_pwd, salt, money)
 
 def insert_book_query(name, author, rent_price):
     return '''INSERT INTO book 
         (author, name, rent_price) VALUES 
-        ({}, {}, {}, {})
+        ('{}', '{}', {})
         '''.format(author, name, rent_price)
 
-with psycopg2.connect('dbname="books_rent" user="postgres" host="localhost" password="12345678"') as conn:
+with psycopg2.connect('dbname=books_rent user=postgres host=localhost password=12345678') as conn:
     cur = conn.cursor()
-    cur.execute(insert_user_query('Василий', '123456', 7500))
-    cur.execute(insert_book_query('Гари Поттер и философский камень', 'Джоан Роулинг', 150))
+    cur.execute("INSERT INTO public.user(username, password, salt, money) VALUES (%(n)s, %(p)s, %(s)s, %(m)s)", dict(n="xxx", p="234234", s="slkdfjslkdjf", m=54))
+    cur.execute(insert_user_query('Василий7', '123456', 7500))
+    # cur.execute(insert_book_query('Гари Поттер и философский камень', 'Джоан Роулинг', 150))
