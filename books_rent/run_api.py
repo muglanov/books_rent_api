7b8c8ec505cbd5336f3259d6b9c09cb40c708ef6
muglanov <muglanov_artem@mail.ru>
2017-12-20 22:56:42 +0300

    route resources for testing

--- books_rent/run_api.py
+++ books_rent/run_api.py
@@ -4,7 +4,15 @@ from books_rent import app, db, api
 from books_rent.resources.book import Book
 
 
-if __name__ == '__main__':
-    db.create_all()
+def route_resources():
     api.add_resource(Book, '/book')
+
+
+def run_api():
+    db.create_all()
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    route_resources()
+    run_api()
