7d6e72846aa2b419f35ace29746037f18dbb65f5
muglanov <muglanov_artem@mail.ru>
2017-12-21 21:03:28 +0300

    fix book resource

--- books_rent/resources/book.py
+++ books_rent/resources/book.py
@@ -1,13 +1,23 @@
 # coding: utf-8
 
-from flask_restful import Resource
+from flask_restful import Resource, request
 
+from books_rent import authorized_users
 from books_rent.models import Book as BookModel
 
 
 class Book(Resource):
 
     def get(self):
+        username = request.args.get('username')
+        token = request.args.get('token')
+        if username is None:
+            return {'status': False, 'error': 'Username is none'}
+        if token is None:
+            return {'status': False, 'error': 'Token is none'}
+        logined_token = authorized_users.get(username)
+        if logined_token is None or logined_token != token:
+            return {'status': False, 'error': 'User is not authorized or session is outdated'}
         books = BookModel.query.all()
         result = []
         for book in books:
@@ -17,4 +27,4 @@ class Book(Resource):
                 'name': book.name,
                 'rent_price': book.rent_price
             })
-        return result
+        return {'status': True, 'error': None, 'books_list': result}
