2f9d9b9b9d36eb5ae7ce0fb3e883e2bef82c380c
muglanov <muglanov_artem@mail.ru>
2017-12-23 00:56:26 +0300

    route rent resource and rename existing routs

--- books_rent/run_api.py
+++ books_rent/run_api.py
@@ -3,12 +3,14 @@
 from books_rent import app, db, api
 from books_rent.resources.book import Book
 from books_rent.resources.user import UserSignIn, UserSignOut
+from books_rent.resources.rent import Rent
 
 
 def route_resources():
     api.add_resource(Book, '/book')
-    api.add_resource(UserSignIn, '/signin')
-    api.add_resource(UserSignOut, '/signout')
+    api.add_resource(UserSignIn, '/sign-in')
+    api.add_resource(UserSignOut, '/sign-out')
+    api.add_resource(Rent, '/rent')
 
 
 def run_api():
