7853d488f3dc83a67cb62afd578b34e0d5b797f0
muglanov <muglanov_artem@mail.ru>
2017-12-23 21:55:47 +0300

    fix remove deprecated authorized users dict

--- books_rent/__init__.py
+++ books_rent/__init__.py
@@ -8,7 +8,6 @@ from books_rent.utils import get_postgresql_db_uri
 username = "postgres"
 pwd = "12345678"
 db_name = 'postgres'
-authorized_users = {}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = get_postgresql_db_uri(username, pwd, db_name)
